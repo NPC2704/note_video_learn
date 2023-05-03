b5f613245e594368c4ea81d2ce455bdbb7c51ba6
Nguyễn Phước Công <88037699+NPC2704@users.noreply.github.com>
2023-05-03 18:13:56 +0700

    opop

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,7 +1,7 @@
 export const apiUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:5000/api"
-    : "https://learnnpcit.onrender.com/api";
+    : "https://learnnpcvideoi.onrender.com/api";
 export const LOCAL_STORAGE_TOKEN_NAME = "learnit-mern";
 export const POSTS_LOADED_SUCCESS = "POSTS_LOADED_SUCCESS";
 export const POSTS_LOADED_FAIL = "POSTS_LOADED_FAIL";
